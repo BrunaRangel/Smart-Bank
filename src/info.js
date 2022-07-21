2e37fbe39e498a58c90d4bcc11b99ea64342e8f5
Bruna Rangel <bruna.henrique.rangel@gmail.com>
2022-07-21 12:37:25 -0300

    adding info.js file

---
+++ src/info.js
@@ -0,0 +1,69 @@
+export const extratoLista = {
+    updates: [
+      {
+        id: "311541",
+        type: "Pizzaria",
+        value: "160,00",
+        date: "22 JUN",
+        from: "Mamma Jamma",
+      },
+      {
+        id: "841312",
+        type: "Transporte",
+        value: "22,00",
+        date: "22 JUN",
+        from: "Uber",
+      },
+      {
+        id: "794162",
+        type: "Outros",
+        value: "230,00",
+        date: "20 JUN",
+        from: "Amazon",
+      },
+      {
+        id: "845520",
+        type: "Transporte",
+        value: "10,00",
+        date: "13 JUN",
+        from: "Uber",
+      },
+      {
+        id: "123131321",
+        type: "Restaurante",
+        value: "150,00",
+        date: "11 JUN",
+        from: "Bonna Parma",
+      },
+      {
+        id: "4564654",
+        type: "Utilidades",
+        value: "130,00",
+        date: "9 JUN",
+        from: "CPFL *energia",
+      },
+      {
+        id: "65445",
+        type: "Saude",
+        value: "15,00",
+        date: "8 JUN",
+        from: "Farma Ponte",
+      },
+      {
+        id: "656565",
+        type: "Transporte",
+        value: "15,00",
+        date: "8 JUN",
+        from: "Uber",
+      },
+  
+      {
+        id: "926544",
+        type: "Outros",
+        value: "150,00",
+        date: "5 JUN",
+        from: "AliExpress",
+      },
+    ],
+  };
+  
