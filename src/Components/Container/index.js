c1ee2cafd7bcdd53d306cebe21f0ce4b5d596102
Bruna Rangel <bruna.henrique.rangel@gmail.com>
2022-07-21 18:20:32 -0300

    adding extract component

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as S from '../Container/styles'
 
 import Account from "../Account";
+import Extract from "../Extract";
 
 
 const Container = () => {
@@ -10,6 +11,7 @@ const Container = () => {
       <S.Titulo>Olá Fulano!</S.Titulo>
       <S.Conteudo>
         <Account />
+        <Extract />
       </S.Conteudo>
     </S.Container>
   );
